7451f0c80ea42c2b2d8cb98733614133dac30bed
Rianna Castro <riannaxcastro@gmail.com>
2021-09-14 09:19:07 -1000

    commit tree a3decf61b2844506f803cb696a06c233096b474e

--- src/index.js
+++ src/index.js
@@ -1,1 +1,44 @@
 console.log('%c HI', 'color: firebrick')
+document.addEventListener("DOMContentLoaded", () => {
+     fetchImage()
+     fetchBreeds()
+})
+
+function fetchImage(){
+    //debugger
+    const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+    fetch(imgUrl)
+    .then(resp => resp.json())
+    .then(dogs => showImage(dogs["message"]))
+}
+
+function showImage(dogs){
+    const divImage = document.getElementById("dog-image-container")
+    dogs.forEach((image) => {
+        divImage.innerHTML += `<img src=${image} />`
+    })
+}
+
+function fetchBreeds(){
+    const breedUrl = 'https://dog.ceo/api/breeds/list/all'
+    fetch(breedUrl)
+    .then(resp => resp.json())
+    .then(dogs => console.log(dogs))
+}
+
+function addBreed(dogs){
+    //console.log(dogs)
+    for (dog in dogs){
+        //console.log(dog)
+        displayABreed(dog)
+    }
+}
+
+function displayABreed(dog){
+    const ul = Document.getElementById("dog-breeds");
+    const li = document.createElement("li");
+    li.innerText = dog;
+    const subUl = document.createElement("ul")
+    
+    ul.appendChild(li)
+}
